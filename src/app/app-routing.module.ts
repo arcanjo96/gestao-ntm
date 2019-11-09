ba807769073e84899d88e23b1809d16557e3b2bd
arcanjo96 <lucasarcanjo.ld@gmail.com>
2019-11-09 00:32:34 -0300

    added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RegisterComponent } from "./register/register.component";
+import { AdminComponent } from "./admin/admin.component";
+import { CardComponent } from "./card/card.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "register", component: RegisterComponent },
+  { path: "admin", component: AdminComponent },
+  { path: "admin/validate", component: CardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { PhoneMaskDirective } from './shared/directives/phone-mask.directive';
-import { CpfMaskDirective } from './shared/directives/cpf-mask.directive';
-import { HttpClientModule } from '@angular/common/http';
-import { CustomMaterialModule } from './shared/custom-material.module';
-import { HeaderComponent } from './header/header.component';
-import { SearchComponent } from './search/search.component';
-import { CardComponent } from './card/card.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { PhoneMaskDirective } from "./shared/directives/phone-mask.directive";
+import { CpfMaskDirective } from "./shared/directives/cpf-mask.directive";
+import { HttpClientModule } from "@angular/common/http";
+import { CustomMaterialModule } from "./shared/custom-material.module";
+import { HeaderComponent } from "./header/header.component";
+import { SearchComponent } from "./search/search.component";
+import { CardComponent } from "./card/card.component";
+import { RouterModule } from "@angular/router";
+import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent, PhoneMaskDirective, CpfMaskDirective, HeaderComponent, SearchComponent, CardComponent
+    AppComponent,
+    PhoneMaskDirective,
+    CpfMaskDirective,
+    HeaderComponent,
+    SearchComponent,
+    CardComponent,
+    RegisterComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
@@ -29,4 +39,4 @@ import { CardComponent } from './card/card.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
